17d3847446f5795996cc825fbe1a13c6918c9939
Roxana Milea <roxy@Roxanas-MacBook-Pro-2.local>
2019-08-03 14:10:31 -0400

    Updates

---
+++ precache-manifest.7f1281c0f562869f7a1e9b7ed12f434b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013fe7bdf00f43e759a87f755f5d940a",
+    "url": "/reactest/index.html"
+  },
+  {
+    "revision": "2f41937a5d4e27d6d1a7",
+    "url": "/reactest/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d31c7a163414c7fa077f",
+    "url": "/reactest/static/css/main.b86b8ecf.chunk.css"
+  },
+  {
+    "revision": "2f41937a5d4e27d6d1a7",
+    "url": "/reactest/static/js/2.784bd99d.chunk.js"
+  },
+  {
+    "revision": "d31c7a163414c7fa077f",
+    "url": "/reactest/static/js/main.546ba754.chunk.js"
+  },
+  {
+    "revision": "43f2d4b69521d766a220",
+    "url": "/reactest/static/js/runtime~main.9432b89b.js"
+  }
+]);
